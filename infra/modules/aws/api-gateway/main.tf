521ccca5a89097afac4bae661cfb8bf88c7ac102
kumagaias <14195829+kumagaias@users.noreply.github.com>
2026-03-15 13:37:54 +0900

    feat: Add /outbreak-data GET endpoint to API Gateway
    - Added outbreak-data resource and GET method
    - Set authorization to NONE for public access
    - Updated deployment triggers to include new endpoint
    - Enables mobile app to fetch real outbreak data from backend

--- infra/modules/aws/api-gateway/main.tf
+++ infra/modules/aws/api-gateway/main.tf
@@ -40,11 +40,36 @@ resource "aws_api_gateway_integration" "lambda" {
   uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
 }
 
+# Outbreak Data endpoint
+resource "aws_api_gateway_resource" "outbreak_data" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "outbreak-data"
+}
+
+resource "aws_api_gateway_method" "get_outbreak_data" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.outbreak_data.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "outbreak_data_lambda" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.outbreak_data.id
+  http_method = aws_api_gateway_method.get_outbreak_data.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
+}
+
 data "aws_region" "current" {}
 
 resource "aws_api_gateway_deployment" "this" {
   depends_on = [
-    aws_api_gateway_integration.lambda
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.outbreak_data_lambda
   ]
 
   rest_api_id = aws_api_gateway_rest_api.this.id
@@ -55,6 +80,9 @@ resource "aws_api_gateway_deployment" "this" {
       aws_api_gateway_resource.generate.id,
       aws_api_gateway_method.post_generate.id,
       aws_api_gateway_integration.lambda.id,
+      aws_api_gateway_resource.outbreak_data.id,
+      aws_api_gateway_method.get_outbreak_data.id,
+      aws_api_gateway_integration.outbreak_data_lambda.id,
     ]))
   }
 
